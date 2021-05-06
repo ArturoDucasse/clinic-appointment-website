1646541ad6e94032c9f3b9a46e09a87a916ea17e
R2D2 <66644638+R2rit0@users.noreply.github.com>
2021-05-06 11:55:00 -0400

    Minor changes

--- src/pages/time/Time.jsx
+++ src/pages/time/Time.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import { Button } from "semantic-ui-react";
 
 const Time = () => {
+  //Todo: Move this to its own file
   const ButtonTest = () => {
     return (
       <div
@@ -13,9 +14,11 @@ const Time = () => {
           border: "1px solid",
           borderRadius: "5px",
           padding: "5px",
+          margin: "10px",
+          boxShadow: "1px 2px  ",
         }}
       >
-        <span>Time here Uhr</span> <span> Open slots</span>
+        <span>Time here</span> <span> Open slots</span>
         <span>{">"}</span>
       </div>
     );
@@ -26,6 +29,14 @@ const Time = () => {
   return (
     <>
       <h1>Wahlen sie die Uhrzeit aus:</h1>
+      <div
+        style={{
+          borderBottom: "2px solid black",
+          width: "400px",
+          margin: "auto",
+          marginBottom: "30px",
+        }}
+      />
       <div
         className="container"
         style={{
@@ -39,17 +50,20 @@ const Time = () => {
           <h2>Morgens</h2>
           <div className="times_container">
             <ButtonTest />
+            <ButtonTest />
           </div>
         </div>
         <div className="Mittags " style={{ width: "350px" }}>
           <h2>Mittags</h2>
           <div className="times_container">
             <ButtonTest />
+            <ButtonTest />
           </div>
         </div>
         <div className="Abends" style={{ width: "350px" }}>
           <h2>Abends</h2>
           <div className="times_container">
+            <ButtonTest />
             <ButtonTest />
           </div>
         </div>
